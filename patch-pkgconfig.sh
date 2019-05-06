b21178e96dd5124e4b3b043594bf7193d81e1af0
Manas <manas@likewyss.com>
2019-05-06 14:20:23 +0530

    Export libintl only on mac

--- patch-pkgconfig.sh
+++ patch-pkgconfig.sh
@@ -1,4 +1,5 @@
 GETTEXT_PREFIX=$1
+OS=$2
 
 cat >$cur__install/lib/pkgconfig/glib-2.0.pc <<EOF
 prefix=${cur__install}
@@ -13,9 +14,9 @@ glib_mkenums=\${bindir}/glib-mkenums
 Name: GLib
 Description: C Utility Library
 Version: 2.61.0
-Libs: -L\${libdir} -lglib-2.0 -L${GETTEXT_PREFIX}/lib -lintl
+Libs: -L\${libdir} -lglib-2.0 $([[ $OS == 'darwin' ]] && echo "-L${GETTEXT_PREFIX}/lib -lintl")
 Libs.private: -Wl,-framework,CoreFoundation -Wl,-framework,Carbon -Wl,-framework,Foundation -Wl,-framework,AppKit -liconv
-Cflags: -I\${includedir}/glib-2.0 -I\${libdir}/glib-2.0/include -I${GETTEXT_PREFIX}/include
+Cflags: -I\${includedir}/glib-2.0 -I\${libdir}/glib-2.0/include $([[ $OS == 'darwin' ]] && echo "-I${GETTEXT_PREFIX}/include")
 EOF
 
 
